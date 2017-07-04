62c18005c73f7f24607e7358f16f8cc96014a4bd
Olivier Wenzek <owenzek@gmail.com>
2017-07-04 21:28:23 +0200

    Templates + context list

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello Explorers!</h1>')
+    name    = 'Gold Nugget'
+    value   = 1000.00
+        
+    return render(request, 'index.html', {'treasures': treasures}) 
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+        
+treasures = [
+    Treasure('Gold Nugget', 500.0, 'gold', "Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fall, CO"),
+    Treasure('Coffee Can', 20.0, 'tin', "ACME, CA"),
+]
