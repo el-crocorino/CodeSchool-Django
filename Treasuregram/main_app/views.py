964ec1b2c08d2b472f73371394f45d4ba6d626e9
Olivier Wenzek <owenzek@gmail.com>
2017-07-05 07:15:28 +0200

    Added static style files and images, updated treasure class to handle img url, updated template

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -9,14 +9,15 @@ def index(request):
     return render(request, 'index.html', {'treasures': treasures}) 
 
 class Treasure:
-    def __init__(self, name, value, material, location):
+    def __init__(self, name, value, material, location, img_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.img_url = img_url
         
 treasures = [
-    Treasure('Gold Nugget', 500.0, 'gold', "Curly's Creek, NM"),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fall, CO"),
-    Treasure('Coffee Can', 20.0, 'tin', "ACME, CA"),
+    Treasure('Gold Nugget', 500.0, 'gold', "Curly's Creek, NM", 'https://vignette3.wikia.nocookie.net/clubpenguin/images/c/cf/Gold_Nugget.png/revision/latest?cb=20131116003237'),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fall, CO", 'http://www.pyritexpertise.com/images/pyrite.png'),
+    Treasure('Coffee Can', 20.0, 'tin', "ACME, CA", 'https://cdn0.rubylane.com/shops/drury/9894-2.1L.jpg'),
 ]
