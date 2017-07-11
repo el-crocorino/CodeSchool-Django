eeb493ba0427a7a8df82efae14ee18cf5a3bbb4c
Olivier Wenzek <owenzek@gmail.com>
2017-07-11 19:01:42 +0200

    Added User field for treasure model

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -5,9 +5,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^([0-9]+)/$', views.detail, name = 'detail'),
+    url(r'^user/(\w+)/$', views.profile, name = 'profile'),
     url(r'^post_url/$', views.post_treasure, name = 'post treasure'),    
+    url(r'^([0-9]+)/$', views.detail, name = 'detail'),
+    url(r'^$', views.index),
 ]
 
 if settings.DEBUG:
