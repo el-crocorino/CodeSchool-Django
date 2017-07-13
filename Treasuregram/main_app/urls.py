d637dbf9f24cd41a055d17d30a6daa683eba352c
Olivier Wenzek <owenzek@gmail.com>
2017-07-13 07:05:19 +0200

    added Login function

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -6,9 +6,10 @@ from . import views
 
 urlpatterns = [
     url(r'^user/(\w+)/$', views.profile, name = 'profile'),
-    url(r'^post_url/$', views.post_treasure, name = 'post treasure'),    
     url(r'^([0-9]+)/$', views.detail, name = 'detail'),
     url(r'^$', views.index),
+    url(r'^post_url/$', views.post_treasure, name = 'post treasure'),    
+    url(r'^login/$', views.login_view, name = 'login'),    
 ]
 
 if settings.DEBUG:
